9649d5f29e4fab0863b839b4e6c73734c1704cef
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-07 17:45:16 -0500

    Fix nav section

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import Navbar from "./Pages/Navbar/Navbar";
 import Hero from "./Pages/Hero/Hero";
 import TechStack from "./Pages/TechStack/TechStack";
-import About from "./Pages/About/About";
+// import About from "./Pages/About/About";
 import Contact from "./Pages/Contact/Contact";
 import Footer from "./Pages/Footer/Footer";
 import Projects from "./Pages/Projects/Projects";
 
 function App() {
     return (
-        <main>
+        <main className="bg-green-200">
             <main className="bg-gradient-to-tl from-primary-50 to-gray-50">
                 <Navbar />
                 <Hero />
             </main>
             <Projects />
             <TechStack />
-            <About />
+            {/* <About /> */}
             <Contact />
             <Footer />
         </main>
--- src/Pages/Navbar/DesktopNavbar.tsx
+++ src/Pages/Navbar/DesktopNavbar.tsx
@@ -21,28 +21,30 @@ export const myLinks = [
 
 const DesktopNavbar = () => {
     return (
-        <nav className="flex justify-around items-center py-6">
-            <Logo />
-            <div>
-                <ul className="nav-links flex gap-8 text-xl">
-                    {myLinks.map((myLink, idx) => {
-                        return (
-                            <li key={idx}>
-                                <a
-                                    className="hover:nav_links"
-                                    href={myLink.link}
-                                >
-                                    {myLink.text}
-                                </a>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-            <button className="py-2 px-6 text-lg font-semibold rounded-xl border-2 border-secondary-300 hover:bg-gray-700 hover:text-white hover:border-gray-700">
-                Connect
-            </button>
-        </nav>
+        <section className="my-0 mx-40">
+            <nav className="flex justify-between items-center py-6">
+                <Logo />
+                <div>
+                    <ul className="nav-links flex gap-8 text-xl">
+                        {myLinks.map((myLink, idx) => {
+                            return (
+                                <li key={idx}>
+                                    <a
+                                        className="hover:nav_links"
+                                        href={myLink.link}
+                                    >
+                                        {myLink.text}
+                                    </a>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+                <button className="py-2 px-6 text-lg font-semibold rounded-xl border-2 border-secondary-300 hover:bg-gray-700 hover:text-white hover:border-gray-700">
+                    Connect
+                </button>
+            </nav>
+        </section>
     );
 };
 
