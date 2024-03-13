3d92c167cadbbbe8d87db67cb384b4228b2c43b3
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-13 19:49:21 -0400

    Remove background color

--- src/Pages/TechStack/TechStack.tsx
+++ src/Pages/TechStack/TechStack.tsx
@@ -14,7 +14,7 @@ const TechStack = () => {
         </h1>
 
         <div className="sm:block sm:gap-0">
-          <div className="mb-8 flex flex-row gap-8 bg-red-300 sm:mb-10 sm:flex sm:flex-col sm:justify-center sm:gap-10 desktop:flex desktop:flex-row desktop:gap-20">
+          <div className="mb-8 flex flex-row gap-8 sm:mb-10 sm:flex sm:flex-col sm:justify-center sm:gap-10 desktop:flex desktop:flex-row desktop:gap-20">
             <div className="rounded-3xl p-5 shadow-2xl desktop:p-10">
               <h2 className="mb-6 text-center text-sm font-semibold text-gray-600 sm:mb-10 sm:text-base desktop:text-3xl">
                 Frontend
@@ -40,7 +40,7 @@ const TechStack = () => {
               </div>
             </div>
           </div>
-          <div className="bg-green-300 sm:flex-col desktop:flex desktop:flex-row desktop:justify-center">
+          <div className="sm:flex-col desktop:flex desktop:flex-row desktop:justify-center">
             <div className="rounded-3xl p-5 shadow-2xl desktop:w-max desktop:p-10">
               <h2 className="mb-6 text-center text-sm font-semibold text-gray-600 sm:mb-10 sm:text-base desktop:text-3xl">
                 Overlap
