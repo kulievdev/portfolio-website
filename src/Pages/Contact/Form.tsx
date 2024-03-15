2efef04fd1af262be71c3d40db16fdbe421e6e37
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-15 06:09:48 -0400

    Change example number

--- src/Pages/Contact/Form.tsx
+++ src/Pages/Contact/Form.tsx
@@ -4,13 +4,13 @@ import Input from "../../Components/Input";
 const Form = () => {
   return (
     <div className="flex flex-1 justify-center rounded-3xl bg-gradient-to-bl from-primary-100 to-secondary-100">
-      <form className=" bigDesktop:p-16 flex w-full flex-col gap-10 p-6 xl:p-12">
+      <form className=" flex w-full flex-col gap-10 p-6 xl:p-12 bigDesktop:p-16">
         <Input type="input" labelText="Name" placeholder="John Doe" />
         <Input type="input" labelText="Email" placeholder="email@example.com" />
         <Input
           type="input"
           labelText="Phone Number"
-          placeholder="(666) 666-6666"
+          placeholder="(222) 222-2222"
         />
         <Input
           type="textarea"
