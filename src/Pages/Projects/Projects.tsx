13dfb16bc425d243b97cb6c281ca6a3cbbb6fff4
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-16 18:03:10 -0400

    Give grid above xl screen size

--- src/Pages/Projects/Projects.tsx
+++ src/Pages/Projects/Projects.tsx
@@ -10,7 +10,9 @@ const Projects = () => {
       <div className="mx-6 flex h-fit flex-col justify-center py-20 sm:mx-10 lg:mx-14 desktop:mx-40">
         <MiniMainHeading text="Browse My Recent" />
         <MainHeading text="Projects" />
-        <div className="my-8 flex flex-wrap justify-center gap-8 sm:grid sm:grid-cols-2 xl:flex">
+        <div className="my-8 flex flex-wrap justify-center gap-8 sm:grid sm:grid-cols-2 xl:grid xl:grid-cols-3">
+          <Project imageSrc={project1} projectName="Project One" />
+          <Project imageSrc={project1} projectName="Project One" />
           <Project imageSrc={project1} projectName="Project One" />
           <Project imageSrc={project1} projectName="Project One" />
           <Project imageSrc={project1} projectName="Project One" />
