efe790eff729ca028c0f2881648403f15d49401c
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-19 10:59:41 -0400

    Fix jumping hamburder icon

--- src/Pages/Navbar/Navbar.tsx
+++ src/Pages/Navbar/Navbar.tsx
@@ -74,49 +74,50 @@ const Navbar = () => {
         </Flex>
       </div>
       {/* Mobile */}
-      <Flex
-        w="100vw"
-        display={display}
-        bgColor="gray.50"
-        zIndex={20}
-        h="100vh"
-        pos="fixed"
-        top="0"
-        left="0"
-        overflowY="auto"
-        flexDir="column"
-      >
-        <Flex justify="flex-end">
-          <IconButton
-            mt={2}
-            mr={6}
-            aria-label="Open Menu"
-            size="lg"
-            icon={<CloseIcon />}
-            onClick={() => changeDisplay("none")}
-          />
-        </Flex>
 
-        <Flex flexDir="column" align="center">
-          {myLinks.map((myLink, idx) => {
-            return (
-              <li key={idx}>
-                <Button
-                  as="a"
-                  onClick={() => changeDisplay("none")}
-                  href={myLink.link}
-                  variant="ghost"
-                  aria-label={myLink.text}
-                  my={5}
-                  w="100%"
-                >
-                  {myLink.text}
-                </Button>
-              </li>
-            );
-          })}
+      <div className="laptop:hidden">
+        <Flex
+          w="100vw"
+          display={display}
+          bgColor="gray.50"
+          zIndex={20}
+          h="100vh"
+          pos="fixed"
+          top="0"
+          left="0"
+          overflowY="auto"
+          flexDir="column"
+        >
+          <div className="mx-10 flex justify-end pt-3 sm:mx-14 sm:py-6 lg:px-14">
+            <IconButton
+              aria-label="Open Menu"
+              size="lg"
+              icon={<CloseIcon />}
+              onClick={() => changeDisplay("none")}
+            />
+          </div>
+
+          <Flex flexDir="column" align="center">
+            {myLinks.map((myLink, idx) => {
+              return (
+                <li key={idx}>
+                  <Button
+                    as="a"
+                    onClick={() => changeDisplay("none")}
+                    href={myLink.link}
+                    variant="ghost"
+                    aria-label={myLink.text}
+                    my={5}
+                    w="100%"
+                  >
+                    {myLink.text}
+                  </Button>
+                </li>
+              );
+            })}
+          </Flex>
         </Flex>
-      </Flex>
+      </div>
     </>
   );
 };
