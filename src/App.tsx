1d2e92b1d81fe01255b328fb40cd84ab8407508a
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-19 15:25:01 -0400

    Remove, does not work

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,8 @@ import About from "./Pages/About/About";
 import Contact from "./Pages/Contact/Contact";
 import Footer from "./Pages/Footer/Footer";
 import Projects from "./Pages/Projects/Projects";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    const cb = () => {
-      document.body.style.visibility = "visible";
-    };
-
-    if (
-      document.readyState === "interactive" ||
-      document.readyState === "complete"
-    ) {
-      cb();
-    } else {
-      document.addEventListener("DOMContentLoaded", cb);
-    }
-
-    return () => {
-      document.removeEventListener("DOMContentLoaded", cb);
-    };
-  }, []);
-
   return (
     <>
       <main id="home" className="bg-gradient-to-t from-primary-50  to-gray-50 ">
