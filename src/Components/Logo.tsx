c0f0422cc42461f702dd5a906b020dee5621f705
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-20 18:30:26 -0400

    remove width and height

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -1,9 +1,7 @@
 import logo from "../assets/Images/logo.png";
 
 const Logo = () => {
-  return (
-    <img alt="Logo" src={logo} className="h-max w-36 cursor-pointer sm:w-48" />
-  );
+  return <img alt="Logo" src={logo} className="w-36 cursor-pointer sm:w-48" />;
 };
 
 export default Logo;
