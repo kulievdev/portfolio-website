ff51bce9f1b098637b4828320ebeea3504f3c382
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-19 22:36:32 -0400

    Fix bug on tech stack page

--- src/Pages/TechStack/TechStack.tsx
+++ src/Pages/TechStack/TechStack.tsx
@@ -25,25 +25,24 @@ const TechStack = () => {
                 delay={0.2}
                 x={"-20%"}
                 y={30}
+                className="rounded-3xl p-6 shadow-2xl desktop:p-10"
               >
-                <div className="rounded-3xl p-6 shadow-2xl desktop:p-10">
-                  <h2 className="mb-6 text-center text-sm  font-semibold text-gray-600 sm:mb-10 sm:text-lg desktop:text-3xl">
-                    Frontend
-                  </h2>
-                  <div className="grid grid-cols-2 flex-wrap gap-12 sm:flex sm:flex-row laptop:grid laptop:grid-cols-5 laptop:flex-nowrap laptop:gap-10">
-                    {myFrontEndStack.map((stack, idx) => {
-                      return (
-                        <Skill
-                          link={() => {
-                            window.open(stack.link);
-                          }}
-                          key={idx}
-                          name={stack.name}
-                          imgSrc={stack.imgSrc}
-                        />
-                      );
-                    })}
-                  </div>
+                <h2 className="mb-6 text-center text-sm  font-semibold text-gray-600 sm:mb-10 sm:text-lg desktop:text-3xl">
+                  Frontend
+                </h2>
+                <div className="grid grid-cols-2 flex-wrap gap-12 sm:flex sm:flex-row laptop:grid laptop:grid-cols-5 laptop:flex-nowrap laptop:gap-10">
+                  {myFrontEndStack.map((stack, idx) => {
+                    return (
+                      <Skill
+                        link={() => {
+                          window.open(stack.link);
+                        }}
+                        key={idx}
+                        name={stack.name}
+                        imgSrc={stack.imgSrc}
+                      />
+                    );
+                  })}
                 </div>
               </AnimateOnScroll>
               <AnimateOnScroll
@@ -53,25 +52,24 @@ const TechStack = () => {
                 scale={0.9}
                 x={"20%"}
                 y={30}
+                className="rounded-3xl p-6 shadow-2xl desktop:p-10"
               >
-                <div className="rounded-3xl p-6 shadow-2xl desktop:p-10">
-                  <h2 className=" mb-6 text-center text-sm font-semibold text-gray-600 sm:mb-10 sm:text-lg desktop:text-3xl">
-                    Backend
-                  </h2>
-                  <div className="grid grid-cols-2 flex-wrap gap-12 sm:flex sm:flex-row laptop:grid laptop:grid-cols-5 laptop:flex-nowrap laptop:gap-10">
-                    {myBackEndStack.map((stack, idx) => {
-                      return (
-                        <Skill
-                          link={() => {
-                            window.open(stack.link);
-                          }}
-                          key={idx}
-                          name={stack.name}
-                          imgSrc={stack.imgSrc}
-                        />
-                      );
-                    })}
-                  </div>
+                <h2 className=" mb-6 text-center text-sm font-semibold text-gray-600 sm:mb-10 sm:text-lg desktop:text-3xl">
+                  Backend
+                </h2>
+                <div className="grid grid-cols-2 flex-wrap gap-12 sm:flex sm:flex-row laptop:grid laptop:grid-cols-5 laptop:flex-nowrap laptop:gap-10">
+                  {myBackEndStack.map((stack, idx) => {
+                    return (
+                      <Skill
+                        link={() => {
+                          window.open(stack.link);
+                        }}
+                        key={idx}
+                        name={stack.name}
+                        imgSrc={stack.imgSrc}
+                      />
+                    );
+                  })}
                 </div>
               </AnimateOnScroll>
             </div>
@@ -82,25 +80,24 @@ const TechStack = () => {
                 delay={0.6}
                 scale={0.9}
                 y={30}
+                className="rounded-3xl p-5 shadow-2xl desktop:w-max desktop:p-10"
               >
-                <div className="rounded-3xl p-5 shadow-2xl desktop:w-max desktop:p-10">
-                  <h2 className="mb-6 text-center text-sm font-semibold text-gray-600 sm:mb-10 sm:text-base desktop:text-3xl">
-                    Overlap
-                  </h2>
-                  <div className="flex flex-row flex-wrap gap-12 laptop:grid laptop:grid-cols-5 laptop:flex-nowrap laptop:gap-10">
-                    {myOverLapStack.map((stack, idx) => {
-                      return (
-                        <Skill
-                          link={() => {
-                            window.open(stack.link);
-                          }}
-                          key={idx}
-                          name={stack.name}
-                          imgSrc={stack.imgSrc}
-                        />
-                      );
-                    })}
-                  </div>
+                <h2 className="mb-6 text-center text-sm font-semibold text-gray-600 sm:mb-10 sm:text-base desktop:text-3xl">
+                  Overlap
+                </h2>
+                <div className="flex flex-row flex-wrap gap-12 laptop:grid laptop:grid-cols-5 laptop:flex-nowrap laptop:gap-10">
+                  {myOverLapStack.map((stack, idx) => {
+                    return (
+                      <Skill
+                        link={() => {
+                          window.open(stack.link);
+                        }}
+                        key={idx}
+                        name={stack.name}
+                        imgSrc={stack.imgSrc}
+                      />
+                    );
+                  })}
                 </div>
               </AnimateOnScroll>
             </div>
