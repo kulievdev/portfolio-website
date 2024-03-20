508a90e196a7646f3d0ea2e2656ab923ee569ade
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-20 17:05:39 -0400

    Give logo height of max

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -1,7 +1,7 @@
 import logo from "../assets/Images/logo.png";
 
 const Logo = () => {
-  return <img src={logo} className="w-36 cursor-pointer sm:w-48" />;
+  return <img src={logo} className="h-max w-36 cursor-pointer sm:w-48" />;
 };
 
 export default Logo;
