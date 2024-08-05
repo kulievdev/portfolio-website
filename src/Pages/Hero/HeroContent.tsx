276007a643847574bb39a2fbda127ec25dc6691c
Tohir Kuliev <kuliev.tech@gmail.com>
2024-08-05 11:54:49 -0400

    Edit hero

--- src/Pages/Hero/HeroContent.tsx
+++ src/Pages/Hero/HeroContent.tsx
@@ -1,5 +1,6 @@
 import Cta from "../../Components/Cta";
 import { motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
 
 const HeroContent = () => {
   return (
@@ -15,12 +16,15 @@ const HeroContent = () => {
       >
         <p className="text-center text-4xl font-bold leading-10 sm:text-5xl sm:font-extrabold sm:leading-[68px] desktop:text-start desktop:text-7xl desktop:leading-[84px]">
           Hello, I'm Tohir a{" "}
-          <span className="bg-gradient-to-r from-primary-700 to-secondary-500 bg-clip-text text-transparent ">
-            Full Stack Web
-          </span>{" "}
-          <span className="bg-gradient-to-r from-primary-700 to-secondary-500 bg-clip-text text-transparent">
-            Developer.
-          </span>{" "}
+          <p className="bg-gradient-to-r from-primary-700 to-secondary-500 bg-clip-text text-transparent ">
+            <Typewriter
+              options={{
+                strings: ["Web Producer.", "Full Stack Web Developer."],
+                autoStart: true,
+                loop: true,
+              }}
+            />{" "}
+          </p>
         </p>
         <p className="text-center text-lg font-medium leading-normal text-gray-600 sm:text-xl desktop:text-start desktop:leading-7">
           Presenting unique and creative software solutions for a variety of
