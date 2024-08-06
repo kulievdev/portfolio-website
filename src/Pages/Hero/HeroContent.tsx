4103058d1ae95b2ec2763e0a1b1e7580df44fad9
Tohir Kuliev <kuliev.tech@gmail.com>
2024-08-06 18:06:33 -0400

    Fix hero text layout issue

--- src/Pages/Hero/HeroContent.tsx
+++ src/Pages/Hero/HeroContent.tsx
@@ -1,6 +1,5 @@
 import Cta from "../../Components/Cta";
 import { motion } from "framer-motion";
-import Typewriter from "typewriter-effect";
 
 const HeroContent = () => {
   return (
@@ -15,19 +14,16 @@ const HeroContent = () => {
         transition={{ duration: 0.6 }}
       >
         <p className="text-center text-4xl font-bold leading-10 sm:text-5xl sm:font-extrabold sm:leading-[68px] desktop:text-start desktop:text-7xl desktop:leading-[84px]">
-          Hello, I'm Tohir a{" "}
-          <p className="bg-gradient-to-r from-primary-700 to-secondary-500 bg-clip-text text-transparent ">
-            <Typewriter
-              options={{
-                strings: ["Web Producer.", "Full Stack Web Developer."],
-                autoStart: true,
-                loop: true,
-              }}
-            />{" "}
-          </p>
+          Hello, I'm Tohir, a{" "}
+          <span className="bg-gradient-to-r from-primary-500 to-secondary-700 bg-clip-text text-transparent ">
+            Web
+          </span>{" "}
+          <span className="bg-gradient-to-r from-secondary-700 to-primary-500 bg-clip-text text-transparent">
+            Producer and Developer.
+          </span>{" "}
         </p>
         <p className="text-center text-lg font-medium leading-normal text-gray-600 sm:text-xl desktop:text-start desktop:leading-7">
-          Presenting unique and creative software solutions for a variety of
+          Presenting unique and creative web solutions for a variety of
           challenges and needs.
         </p>
       </motion.header>
