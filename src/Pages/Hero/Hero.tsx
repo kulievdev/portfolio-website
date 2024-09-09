90c5f220c62a93a58623f83b726fa9d1b8be849f
Tohir Kuliev <kuliev.tech@gmail.com>
2024-09-09 15:59:45 -0400

    fix small layout issue

--- src/Pages/Hero/Hero.tsx
+++ src/Pages/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import HeroContent from "./HeroContent";
 
 const Hero = () => {
   return (
-    <section className="section_container mx-6 flex h-[100vh] flex-col-reverse items-center justify-center sm:mx-10 sm:mx-12 sm:gap-10 largeTablet:flex-row largeTablet:justify-between lg:mx-28 laptop:flex  desktop:mx-60 ">
+    <section className="section_container mx-6 flex h-[100vh] flex-col-reverse items-center justify-center sm:mx-10 sm:mx-12 sm:gap-10 largeTablet:flex-row largeTablet:justify-between lg:mx-28 desktop:mx-60 ">
       <HeroContent />
       <HeroImage />
     </section>
