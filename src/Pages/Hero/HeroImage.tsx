60337577def27e68e9821323ef0dc6418c6fce4b
Tohir Kuliev <kuliev.tech@gmail.com>
2024-09-12 21:35:24 -0400

    fix image bug

--- src/Pages/Hero/HeroImage.tsx
+++ src/Pages/Hero/HeroImage.tsx
@@ -13,7 +13,7 @@ const HeroImage = () => {
     <AnimateOnScroll x="10%" delay={0} scale={0.9} duration={0.6} notLazy>
       <div className="sm:w-max largeTablet:relative largeTablet:mx-0 largeTablet:my-auto largeTablet:flex">
         <img
-          className="tablet:w-[350px]h-[160px] w-[160px] rounded-full object-cover ring-4 ring-primary-200 ring-offset-4  xs:h-[200px] xs:w-[200px] sm:h-[250px] sm:w-[250px] sm:ring-8 sm:ring-primary-200 sm:ring-offset-8 md:h-[300px] md:w-[300px] largeTablet:relative largeTablet:z-10 largeTablet:h-[350px] largeTablet:w-[250px] largeTablet:cursor-pointer largeTablet:rounded-3xl largeTablet:ring-0 largeTablet:ring-offset-0 tablet:h-[450px] tablet:w-[350px] desktop:h-[500px] desktop:w-[400px]"
+          className="h-[160px] w-[160px] rounded-full object-cover ring-4 ring-primary-200 ring-offset-4 xs:h-[200px]  xs:w-[200px] sm:h-[250px] sm:w-[250px] sm:ring-8 sm:ring-primary-200 sm:ring-offset-8 md:h-[300px] md:w-[300px] largeTablet:relative largeTablet:z-10 largeTablet:h-[350px] largeTablet:w-[250px] largeTablet:cursor-pointer largeTablet:rounded-3xl largeTablet:ring-0 largeTablet:ring-offset-0 tablet:h-[450px] tablet:w-[350px] tablet:w-[350px] desktop:h-[500px] desktop:w-[400px]"
           src={profilePic}
           alt="Tohir Kuliev Profile Picture"
           onMouseDown={handleImageHover}
