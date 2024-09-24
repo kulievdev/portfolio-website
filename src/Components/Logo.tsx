93f3ed657a5539204c48b61165e22d6e95dde4c9
Tohir Kuliev <kuliev.tech@gmail.com>
2024-09-23 20:23:07 -0400

    give smaller size for smaller screens on logo

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -1,7 +1,13 @@
 import logo from "../assets/Images/logo.png";
 
 const Logo = () => {
-  return <img alt="Logo" src={logo} className="w-24 cursor-pointer" />;
+  return (
+    <img
+      alt="Logo"
+      src={logo}
+      className="w-20 cursor-pointer largeTablet:w-24"
+    />
+  );
 };
 
 export default Logo;
