d0157171bc80b06d093d374bb7ba0ccbebdfe6b5
Tohir Kuliev <kuliev.tech@gmail.com>
2024-09-23 20:16:10 -0400

    Resize logo

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -1,7 +1,7 @@
 import logo from "../assets/Images/logo.png";
 
 const Logo = () => {
-  return <img alt="Logo" src={logo} className="w-20 cursor-pointer" />;
+  return <img alt="Logo" src={logo} className="w-24 cursor-pointer" />;
 };
 
 export default Logo;
